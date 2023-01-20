4fc024b2f0caaebd4c1708edd00e654c4d3c990d
EC2 Default User <ec2-user@ip-172-31-12-57.ap-northeast-1.compute.internal>
2023-01-20 11:34:53 +0000

    “test”

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
   end
 
+  
   test "should get help" do
     get static_pages_help_url
     assert_response :success
